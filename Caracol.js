1f6231628a966b3e62465e9502182ea3265d0388
Mons1214 <mons.gorsor@gmail.com>
2025-09-18 13:40:19 -0600

    A song has been added to the page, the stop and restart buttons pause and play the song, and the styles have been modified.

--- Caracol.js
+++ Caracol.js
@@ -1,32 +1,57 @@
+
+
 const celdas = document.querySelectorAll(".celda");
 
-const order = [0,1,2,5,8,7,6,3,4];
+const order = [0, 1, 2, 5, 8, 7, 6, 3, 4];
 
 let actualIndex = 0;
 
+
+
 function mostrarCelda() {
-    celdas.forEach(celda => celda.classList.remove("active"));
+    celdas.forEach(celda => {
+        celda.classList.remove("active");
+        celda.textContent = "";
+    });
     const actualCelda = order[actualIndex];
     celdas[actualCelda].classList.add("active");
+    celdas[actualCelda].textContent = "🐌";
 }
 
-function recorrer () {
+
+
+function recorrer() {
     mostrarCelda();
     actualIndex = (actualIndex + 1) % order.length;
 }
 
+
 let intervalId = setInterval(recorrer, 1000);
+
 
 document.getElementById("stop").addEventListener("click", () => {
     clearInterval(intervalId);
     intervalId = null;
+    // Detener la canción
+    const audio = document.getElementById("audio-cancion");
+    if (audio && !audio.paused) {
+        audio.pause();
+    }
 });
+
 
 document.getElementById("reiniciar").addEventListener("click", () => {
     if (!intervalId) {
         intervalId = setInterval(recorrer, 1000);
     }
+    // Reproducir la canción
+    const audio = document.getElementById("audio-cancion");
+    if (audio && audio.paused) {
+        audio.play();
+    }
 });
+
+
 
 document.getElementById("next").addEventListener("click", () => {
     clearInterval(intervalId);
@@ -36,11 +61,14 @@ document.getElementById("next").addEventListener("click", () => {
     mostrarCelda();
 });
 
+
+
 document.getElementById("retroceder").addEventListener("click", () => {
     clearInterval(intervalId);
     intervalId = null;
     actualIndex = (actualIndex - 1 + order.length) % order.length;
     mostrarCelda();
 })
+
 
 mostrarCelda();
